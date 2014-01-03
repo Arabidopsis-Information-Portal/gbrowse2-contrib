40597dec06648456d34412b198fc6ef20b627d2e
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-01-02 19:04:31 -0500

    Added the spliced junctions, repeats and tDNA tracks, removed the phosphorylation track. updated the loading shell script with appropriate paths to custom data (in sync with the `AIP/araport-contrib` repo)

--- gbrowse2-load.sh
+++ gbrowse2-load.sh
@@ -3,27 +3,33 @@
 ## Set Path variables
 AIP_HOME=/usr/local/projects/AIP
 TAIR_DATA=DataProviders/TAIR
-GBROWSE_DATA_ROOT=ftp.arabidopsis.org/Maps/gbrowse_data
-CUSTOM_DATA=vkrishna/TAIR10_GFF3/genome_browser
+TAIR10_RELEASE=ftp.arabidopsis.org/Genes/TAIR10_genome_release
+TAIR9_RELEASE=ftp.arabidopsis.org/Genes/TAIR9_genome_release
+TAIR10_gff3=$AIP_HOME/$TAIR_DATA/$TAIR10_RELEASE/TAIR10_gff3
+TAIR9_gff3=$AIP_HOME/$TAIR_DATA/$TAIR9_RELEASE/TAIR9_gff3
+
+## Custom data paths
+TAIR10_custom_gff3=$AIP_HOME/$TAIR_DATA/custom_data/$TAIR10_RELEASE/TAIR10_gff3
+TAIR9_custom_gff3=$AIP_HOME/$TAIR_DATA/custom_data/$TAIR9_RELEASE/TAIR9_gff3
+WHOLE_CHRS=$AIP_HOME/$TAIR_DATA/custom_data/Sequences/whole_chromosomes
 
 /usr/local/packages/perl/bin/bp_seqfeature_load.pl \
     -d 'dbi:Pg:database=[DATABASE];host=[HOSTNAME]' -a 'DBI::Pg' \
-    -u [USRENAME] -p [PASSWORD] -c -v \
-    -T /usr/local/scratch/EUK/vkrishna/tmp \
-    $AIP_HOME/vkrishna/TAIR10_fasta/TAIR10_genome.fas \
-    $AIP_HOME/$CUSTOM_DATA/TAIR10_GFF3_genes_transposons.20131107_1826.gff3 \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR10/TAIR10_unconfirmed_exons.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR10/Spliced_Junctions_clustered.gff \
-    $AIP_HOME/$CUSTOM_DATA/tair9_Quesneville_Transposons_20090429.20131118_0936.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR10/TAIR_GFF3_ssrs.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Assembly_GFF/TAIR9_GFF3_assemblies.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Assembly_GFF/tair9_Assembly_gaps.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Assembly_GFF/tair9_assembly_updates.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Variation_GFF/TAIR9_GFF3_tdnas.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Variation_GFF/TAIR9_GFF3_polymorphisms.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Variation_GFF/TAIR9_GFF3_markers.gff \
-    $AIP_HOME/$CUSTOM_DATA/tair9_smallRNA_17_summary.fixed.gff \
-    $AIP_HOME/$TAIR_DATA/$GBROWSE_DATA_ROOT/TAIR9/Expression_GFF/tair9_ATH1-121501.gff \
-    $AIP_HOME/$CUSTOM_DATA/tair9_atproteometair7.20131118_0936.gff \
-    $AIP_HOME/$CUSTOM_DATA/tair9_Briggs_atproteome7_20090401.20131118_0936.gff \
+    -u [USERNAME] -p [PASSWORD] -c -v \
+    -T /tmp \
+    $WHOLE_CHRS/TAIR10_genome.fas \
+    $TAIR10_custom_gff3/TAIR10_GFF3_genes_transposons.gff3 \
+    $TAIR10_gff3/TAIR10_unconfirmed_exons.gff \
+    $TAIR10_gff3/Spliced_Junctions_clustered.gff \
+    $TAIR9_custom_gff3/tair9_Quesneville_Transposons_20090429.gff \
+    $TAIR10_gff3/TAIR_GFF3_ssrs.gff \
+    $TAIR9_gff3/Assembly_GFF/TAIR9_GFF3_assemblies.gff \
+    $TAIR9_gff3/Assembly_GFF/tair9_Assembly_gaps.gff \
+    $TAIR9_gff3/Assembly_GFF/tair9_assembly_updates.gff \
+    $TAIR9_gff3/Variation_GFF/TAIR9_GFF3_tdnas.gff \
+    $TAIR9_gff3/Variation_GFF/TAIR9_GFF3_polymorphisms.gff \
+    $TAIR9_gff3/Variation_GFF/TAIR9_GFF3_markers.gff \
+    $TAIR9_gff3/Expression_GFF/tair9_ATH1-121501.gff \
+    $TAIR9_gff3/tair9_atproteometair7.gff \
+    $TAIR9_gff3/tair9_Briggs_atproteome7_20090401.20131118_0936.gff \
     > gbrowse2-load.log 2> gbrowse2-load.err
