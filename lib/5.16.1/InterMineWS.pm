0c16783d209a766a6b3a34c8c795ad33f839210a
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-03-27 11:39:22 -0400

    Update balloon popup HTML with more fields (to match updated content in Intermine); slight modifications to config: set initial landmark, add strandedness to Protein Coding Genes track; track changes made to header line in cgi-bin/gb2/gbrowse

--- lib/5.16.1/InterMineWS.pm
+++ lib/5.16.1/InterMineWS.pm
@@ -3,24 +3,51 @@ use strict;
 use warnings;
 
 sub balloon_popup {
-    my ($feature) = @_;
+    my ($feature)    = @_;
     my $thalemineURL = "$ENV{SERVER_NAME}/thalemine";
-    my @elems = ('primaryIdentifier', 'Alias', 'curatorSummary', 'computationalDescription');
+    my %elements        = (
+        'symbol'                   => 'Gene Symbol',
+        'alias'                    => 'Gene Aliases',
+        'curatorSummary'           => 'Curator Summary',
+        'computationalDescription' => 'Computational Description',
+        'confClass'                => 'Confidence Class (scale of 1-10)',
+        'confRating'               => 'Confidence Rating (scale of 1-5 stars)'
+    );
 
     my $primaryIdentifier = $feature->name;
     my ($type, $source) = $feature->type =~ /^(\S+):(\S+)$/;
-    $type =~ s/(\S+)/\U$1/gs;
-    $type =~ s/_(\S{1})/\U$1/gs;
+    $type =~ s/(\S+)/\u$1/gs;
+    $type =~ s/_(\S{1})/\u$1/gs;
 
     use Webservice::InterMine;
     my $service = Webservice::InterMine->get_service("$thalemineURL");
-    my $qresult = $service->new_query(class => $type)->select('*')
-      ->where(primaryIdentifier => [$primaryIdentifier])->first;
-    my @results = ();
-    foreach my $elem (@elems) {
-        push @results, $qresult->{$elem} if (defined $qresult->{$elem});
+    my $query = $service->new_query(class => $type);
+    $query->add_view(qw(*));
+    $query->add_constraint(
+        path  => "$type.primaryIdentifier",
+        op    => "=",
+        value => "$primaryIdentifier",
+        code  => "A",
+    );
+    my $ri = $query->results_iterator();
+    die $ri->status_line unless $ri->is_success;
+
+    my @resultsHtml = ();
+    while(my $row = <$ri>) {
+        foreach my $column (keys %elements) {
+            if (defined $row->{$column}) {
+                push @resultsHtml, '<tr class="popuptbl">', "<td><strong>$elements{$column}</strong></td>",
+                  "<td>" . $row->{$column} . "</td>", "</tr>";
+            }
+        }
     }
-    return "<center>" . (join "<br />", @results) . "</center>";
+    $ri->close();
+
+    if(scalar @resultsHtml > 0) {
+        unshift @resultsHtml, "<table border=1>";
+        push @resultsHtml, "</table>";
+        return join "", @resultsHtml;
+    }
 }
 
 1;
